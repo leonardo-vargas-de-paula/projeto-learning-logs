686301b5f25fead670c4601381cdccf5b87f4159
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-04-07 16:09:24 -0400

    feat: adiciona topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Topic(models.Model):
+    """Assunto que o usuário está aprendendo."""
+    text = models.CharField(max_length=200)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma string representando o modelo."""
+        return self.text
